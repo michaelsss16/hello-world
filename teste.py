afc98be537d9c8ebdbacdca9cd56510b524f0dc8
michael-silva-dti <91736102+michael-silva-dti@users.noreply.github.com>
2021-10-05 09:18:28 -0300

    Adicionando modificações na primeira branch

--- teste.py
+++ teste.py
@@ -1,2 +1,6 @@
-    print("mensagem da primeirabranch")
-    print("Adicionando mais uma mensagem para teste")
+print("mensagem da primeirabranch - versão 2")
+print("Adicionando mais uma mensagem para teste")
+def retornaMensagem():
+    print("Agora eu estou criando mais uma funç~~ao para retornar uma mensagem nessa branch")
+retornaMensagem()
+    
